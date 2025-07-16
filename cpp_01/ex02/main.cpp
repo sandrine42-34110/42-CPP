a692cc0dc919e3eb0444a43a930b18f400e4580d
sandrine <sandrinepupier@gmail.com>
2025-07-16 16:26:36 +0200

    deb ex02

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sapupier <sapupier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 09:10:41 by sapupier          #+#    #+#             */
+/*   Updated: 2025/07/16 16:24:18 by sapupier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string variable = "HI THIS IS BRAIN";
+
+
+	// std::cout << "--- Test N = 5 ---" << std::endl;
+	// int N = 5;
+	
+	// Zombie* horde = zombieHorde(N, "Foo");
+	
+	// for (int i = 0; i < N; i++)
+	// {
+	// 	horde[i].announce();
+	// }
+	// delete []horde;
+
+	// std::cout << std::endl;
+	// std::cout << "--- Test N = 0 ---" << std::endl;
+	// N = 0;
+	
+	// Zombie* emptyHorde = zombieHorde(N, "Nobody");
+	
+	// for (int i = 0; i < N; i++)
+	// {
+	// 	emptyHorde[i].announce();
+	// }
+	// delete []emptyHorde;
+
+	// std::cout << std::endl;
+	// std::cout << "--- Test N = 3 ---" << std::endl;
+	// N = 3;
+	
+	// Zombie* secondHorde = zombieHorde(N, "Boo");
+	
+	// for (int i = 0; i < N; i++)
+	// {
+	// 	secondHorde[i].announce();
+	// }
+	// delete []secondHorde;
+	return (0);
+}
